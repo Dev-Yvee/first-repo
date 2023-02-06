454bf184221c60dd6e3ed31fe4caacc21f0e1d8f
YVONE1999 <njeryvone15@gmail.com>
2023-02-06 16:47:29 +0300

    new

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 var express = require("express");
 var app = express()
-var students=["Tony","Lisa","Michael","Ginger","Food"];
+//var students=["Tony","Lisa","Michael","Ginger","Food"];
 // var cars={Brand:"Volvo",Model:"Mustang",Year:2006};
 // var carbrand=cars.Year
 //     console.log(carbrand)
     
 var student=[{ID:1,FirstName:"Yvone",SecondName:"Njeri",StudentID:8391},{ID:2,FirstName:"Benson",SecondName:"Gathu",StudentID:8392},{ID:3,FirstName:"Aburili",SecondName:"Anami",StudentID:8393}];
-var studentname=student.FirstName("Yvone")
-console.log(studentname)
 
 var books=[{ID:10,Title:"Tommorrow People",SerialNumber:100},{ID:11,Title:"River and the source",SerialNumber:200},{ID:12,Title:"River Between",SerialNumber:300}]
 
@@ -26,15 +24,15 @@ app.get("/student", (req, res, next) => {
 //     res.json(cars);
 //     });
 
-app.get("/students", (req, res, next) => {
-res.json(students);
-});
+//app.get("/students", (req, res, next) => {
+//res.json(students);
+//});
 app.get("/name", (req, res, next) => {
     res.json(["Yvone ","njeri","Yvee"]);
    });
-app.get("/phones" ,(req, res, next) => {
-    res.json(["Samsung ","Techno","IPhone"]);
-   });
+//app.get("/phones" ,(req, res, next) => {
+  //  res.json(["Samsung ","Techno","IPhone"]);
+   //});
 
 
 

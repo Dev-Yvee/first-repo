633a785c00de62b005fa660cb23e7110960a6674
YVONE1999 <njeryvone15@gmail.com>
2023-02-08 17:37:23 +0300

    POST method

--- index.js
+++ index.js
@@ -1,31 +1,71 @@
 var express = require("express");
 var app = express();
+var bodyParser = require("body-parser");
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 //var students=["Tony","Lisa","Michael","Ginger","Food"];
 // var cars={Brand:"Volvo",Model:"Mustang",Year:2006};
 // var carbrand=cars.Year
 //     console.log(carbrand)
 
-var student = [
+var students = [
   { ID: 1, FirstName: "Yvone", SecondName: "Njeri", StudentID: 8391 },
   { ID: 2, FirstName: "Benson", SecondName: "Gathu", StudentID: 8392 },
   { ID: 3, FirstName: "Aburili", SecondName: "Anami", StudentID: 8393 },
 ];
 
 var books = [
-  { ID: 10, Title: "Tommorrow People", SerialNumber: 100 },
-  { ID: 11, Title: "River and the source", SerialNumber: 200 },
-  { ID: 12, Title: "River Between", SerialNumber: 300 },
+  { ID: 10, Title: "Tommorrow People", SerialNumber: "SN1100" },
+  { ID: 11, Title: "River and the source", SerialNumber: "SN100" },
+  { ID: 12, Title: "River Between", SerialNumber: "SN300" },
+  { ID: 13, Title: "Betrayal in the city", SerialNumber: "SN544" },
 ];
 
-var bookDetails = function (bookId) {
-  for (book of books) {
-    if (book["ID"] === parseInt(bookId)) {
-      return book;
-    }else{
-        return `Book with ID ${bookId} doesnt exist`
+function studentdetails(studentid) {
+  for (let i = 0; i < students.length; i++) {
+    if (students[i]["StudentID"] == studentid) {
+      //console.log(studentid)
+      return students[i];
     }
+    //  else
+    //  {
+    //    return "Student with id ${studentid} does not exist"
+    //   }
   }
- 
+}
+
+// for (student of students) {
+//   //for loop iterating the student list
+//   if (student["StudentID"] == parseInt(studentid)) {
+//     //if loop cheching if the studentid input is true or false
+//     console.log(student)
+//     // return student;
+//   } else {
+//     return `Student with that id ${studentid} does not exist`;
+//   }
+// }
+
+var bookDetails = (bookId) => {
+  for (let i = 0; i < books.length; i++) {
+    if (books[i]["SerialNumber"] == bookId) {
+      // console.log(books[i])
+      return books[i];
+    }
+    // else {
+    //   return `Book with the id ${bookId} does not exist`
+    // }
+  }
+
+  // for (book of books) {
+  //   if (book["SerialNumber"] === bookId) {
+  //     return book;
+  //   } else {
+  //     return `Book with Serial Number ${bookId} doesnt exist`;
+  //   }
+
   // res.json(`my book id is ${bookId}`);
   // console.log(`my book id is ${bookId}`);
 
@@ -37,20 +77,34 @@ app.get("/books", (req, res, next) => {
 });
 
 app.get("/books/:id", (req, res) => {
+  //checks the specific book id
   const bookId = req.params.id;
- var bookInfo =  bookDetails(bookId);
+
+  var bookInfo = bookDetails(bookId);
   res.json(bookInfo);
 });
+
+
+app.post("/books", (req, res) => {
+  var { ID, Title, SerialNumber } = req.body;
+  var book = req.body;
+  books.push(book);
+  return res.send("Book has been succesfully added");
+});
+
+
 
 app.get("/students", (req, res, next) => {
   res.json(student);
 });
 
 app.get("/students/:id", (req, res) => {
-  //identified the specific student   :id is a variable(input a)
-  const id = req.params.id;
-  res.json(`my students id is ${id}`); // browser output
-  console.log(`my book id is ${id}`); //terminal output
+  //identified the specific student   :id is a variable(input a number in the browser)
+  const studentid = req.params.id;
+  var studentinfo = studentdetails(studentid); //calling the function studentdetails
+  res.json(studentinfo);
+  // res.json(`my students id is ${id}`); // browser output
+  // console.log(`my book id is ${id}`); //terminal output
 });
 
 // app.get("/cars", (req, res, next) => {

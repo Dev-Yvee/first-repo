fe37376517003aa58969e9928f1beb361e8d6510
YVONE1999 <njeryvone15@gmail.com>
2023-02-08 17:43:39 +0300

    body parser added

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 
-app.use(express.json());
+//app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
